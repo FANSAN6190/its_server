cb81dce53157c3b507eeecda3c4ce84f0f3e1994
fansan6190 <fanin.s.pbl@gmail.com>
2024-02-23 17:14:57 +0530

    spring boot server for ITS

---
+++ ITS_server/src/main/java/com/example/ITS_server/ItsServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ITS_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItsServerApplication.class, args);
+	}
+
+}
---
+++ ITS_server/src/test/java/com/example/ITS_server/ItsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ITS_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
