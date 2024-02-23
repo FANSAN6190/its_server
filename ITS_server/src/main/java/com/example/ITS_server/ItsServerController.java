72138727a41f866eec44ade9adfce8c247f60513
fansan6190 <fanin.s.pbl@gmail.com>
2024-02-23 17:56:58 +0530

    updated server build to gradle

--- ITS_server/src/main/java/com/example/ITS_server/ItsServerController.java
+++ ITS_server/src/main/java/com/example/ITS_server/ItsServerController.java
@@ -1,15 +1,25 @@
 package com.example.ITS_server;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 
 public class ItsServerController {
+    private final AtomicLong counter = new AtomicLong();
+
     @GetMapping("/")
     public String hello() {
         return "This is ITS server API.";
+    }
+
+    @GetMapping("/ping")
+    public String ping(@RequestParam(value = "message", defaultValue = "default") String name) {
+        return String.format("Check : %s , Received : %s", counter.incrementAndGet(), name);
     }
 }
